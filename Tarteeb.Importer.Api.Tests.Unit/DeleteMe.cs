d38fcce27cf9835215e87452e538714a86627faa
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-10-07 22:33:12 +0900

    INFRA: Initialize Unit Test Project

---
+++ Tarteeb.Importer.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,15 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using Xunit;
+
+namespace Tarteeb.Importer.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
