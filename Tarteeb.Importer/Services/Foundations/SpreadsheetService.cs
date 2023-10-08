2be2ffa7c9d44cddee5abd129bcdeb41f59a2102
Zafar <icom@MACKBOOK>
2023-10-08 17:49:19 +0300

    FOUNDATIONS: Get All Applicants

---
+++ Tarteeb.Importer/Services/Foundations/SpreadsheetService.cs
@@ -0,0 +1,37 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using Bytescout.Spreadsheet;
+using System.Collections.Generic;
+using Tarteeb.Importer.Models.Applicants;
+using Tarteeb.Provider.Brokers.Spreadsheets;
+
+namespace Tarteeb.Importer.Services.Foundations
+{
+    public class SpreadsheetService
+    {
+        SpreadsheetBroker spreadsheetBroker = new SpreadsheetBroker();
+
+        public List<Applicant> GetAllApplicants(string filePath)
+        {
+            List<Applicant> allApplicants = new List<Applicant>();
+
+            Spreadsheet document = new Spreadsheet();
+
+            document.LoadFromFile(filePath);
+
+            Worksheet worksheet = document.Workbook.Worksheets[0];
+
+            for (int row = 1; row <= worksheet.UsedRangeRowMax; row++)
+            {
+                var applicant = spreadsheetBroker.ImportApplicantFromExcel(filePath, row);
+
+                allApplicants.Add(applicant);
+            }
+
+            return allApplicants;
+        }
+    }
+}
