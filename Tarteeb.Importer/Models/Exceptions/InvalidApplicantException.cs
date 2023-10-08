6084ad0585b90f2169acf78446d2ee4bea990c5a
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-10-08 22:19:38 +0900

    PROCESSINGS: Validate All Applicants

---
+++ Tarteeb.Importer/Models/Exceptions/InvalidApplicantException.cs
@@ -0,0 +1,16 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using Xeptions;
+
+namespace Tarteeb.Importer.Models.Exceptions
+{
+    public class InvalidApplicantException : Xeption
+    {
+        public InvalidApplicantException()
+            : base(message: "Applicant is invalid")
+        { }
+    }
+}
