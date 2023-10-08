86d71ac096a98c9bd8f6f09a161133099e6aa1c7
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-10-08 23:00:26 +0900

    BROKERS: Insert Group

--- Tarteeb.Importer/Brokers/Storages/StorageBroker.Groups.cs
+++ Tarteeb.Importer/Brokers/Storages/StorageBroker.Groups.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Tarteeb.Importer.Models.Groups;
 
 namespace Tarteeb.Importer.Brokers.Storages
@@ -6,5 +7,13 @@ namespace Tarteeb.Importer.Brokers.Storages
     public partial class StorageBroker
     {
         DbSet<Group> Groups { get; set; }
+
+        public async Task<Group> InsertGroupAsync(Group group)
+        {
+            await this.Groups.AddAsync(group);
+            await this.SaveChangesAsync();
+
+            return group;
+        }
     }
 }
