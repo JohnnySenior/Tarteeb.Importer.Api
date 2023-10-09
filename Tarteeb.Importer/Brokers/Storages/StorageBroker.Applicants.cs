28de76ee982c86b82c1a5eb95732771ad4517b6a
Zafar <icom@MACKBOOK>
2023-10-09 07:13:49 +0300

    BROKERS: Insert Applicant

--- Tarteeb.Importer/Brokers/Storages/StorageBroker.Applicants.cs
+++ Tarteeb.Importer/Brokers/Storages/StorageBroker.Applicants.cs
@@ -4,6 +4,7 @@
 //===========================
 
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 using Tarteeb.Importer.Models.Applicants;
 
 namespace Tarteeb.Importer.Brokers.Storages
@@ -11,5 +12,13 @@ namespace Tarteeb.Importer.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<Applicant> Applicants { get; set; }
+
+        public async Task<Applicant> InsertApplicantAsync(Applicant applicant)
+        {
+            await this.Applicants.AddAsync(applicant);
+            await this.SaveChangesAsync();
+
+            return applicant;
+        }
     }
 }
