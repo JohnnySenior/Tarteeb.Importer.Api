c55318b6adf3011564abdf0730aed975253541cd
Zafar <icom@MACKBOOK>
2023-10-09 15:59:48 +0300

    PROCESSINGS: Add Applicant

---
+++ Tarteeb.Importer/Services/Processings/ProcessingApplicantService.cs
@@ -0,0 +1,31 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tarteeb.Importer.Models.Applicants;
+using Tarteeb.Importer.Services.Foundations;
+
+namespace Tarteeb.Importer.Services.Processings
+{
+    public class ProcessingApplicantService
+    {
+        private readonly ApplicantService applicantService;
+
+        public ProcessingApplicantService(ApplicantService applicantService)
+        {
+            this.applicantService = applicantService;
+        }
+
+        public async Task<List<Applicant>> AddApplicantAsync(List<Applicant> applicants)
+        {
+            foreach (var applicant in applicants)
+            {
+                await this.applicantService.AddApplicantAsync(applicant);
+            }
+            return applicants;
+        }
+    }
+}
