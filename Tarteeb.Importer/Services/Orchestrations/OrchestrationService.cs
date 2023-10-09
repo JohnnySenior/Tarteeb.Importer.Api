1838c283b36c7d4d8911233636ee669a7a6ee170
Zafar <icom@MACKBOOK>
2023-10-09 16:39:02 +0300

    ORCHESTRATIONS: Get All Validated Applicants

---
+++ Tarteeb.Importer/Services/Orchestrations/OrchestrationService.cs
@@ -0,0 +1,55 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//===========================
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tarteeb.Importer.Models.Applicants;
+using Tarteeb.Importer.Services.Processings;
+
+namespace Tarteeb.Importer.Services.Orchestrations
+{
+    public class OrchestrationService
+    {
+        private readonly SpreadsheetProcessingService spreadsheetProcessingService;
+        private readonly GroupProcessingService groupProcessingService;
+        private readonly ProcessingApplicantService applicantProcessningService;
+
+        List<Applicant> readyApplicants = new List<Applicant>();
+        List<Applicant> fullAplicants = new List<Applicant>();
+
+        public OrchestrationService(
+            SpreadsheetProcessingService spreadsheetProcessingService,
+            GroupProcessingService groupProcessingService,
+            ProcessingApplicantService applicantProcessningService)
+        {
+            this.spreadsheetProcessingService = spreadsheetProcessingService;
+            this.groupProcessingService = groupProcessingService;
+            this.applicantProcessningService = applicantProcessningService;
+        }
+
+        private async Task<List<Applicant>> InsertedApplicant(string filePath)
+        {
+            var readyApplicants = await FullApplicants(filePath);
+
+            var insertedApplicants = await
+                applicantProcessningService.AddApplicantAsync(readyApplicants);
+
+            return insertedApplicants;
+        }
+
+        private async Task<List<Applicant>> FullApplicants(string filePath)
+        {
+            var importedApplicants = ImportFromExcel(filePath);
+
+            fullAplicants = await
+                groupProcessingService.AllApplicants(importedApplicants);
+
+            return fullAplicants;
+        }
+
+        private List<Applicant> ImportFromExcel(string filePath) =>
+            spreadsheetProcessingService.ValidateInvalidApplicants(filePath);
+    }
+}
